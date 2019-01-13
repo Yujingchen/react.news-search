cadbab3ecf03f4c27d69f25e629c9dab7b0376c0
Chen Yujing <vit7david@outlook.com>
2019-01-13 19:35:00 +0200

    added search filed

---
+++ src/components/layout/Header1.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+// import { Link } from "react-router-dom";
+import "../../App.css";
+
+// function debounce(func, wait = 1000) {
+//   let timeout;
+//   return function() {
+//     let args = arguments;
+//     let context = this;
+//     clearTimeout(timeout);
+//     timeout = func.apply(context, args);
+//   };
+// }
+
+class Header1 extends Component {
+  // constructor() {
+  //   super();
+  //   this.search = this.search.bind(this);
+  // }
+  render() {
+    return (
+      <div className="container">
+        <nav className=" navbar navbar-expand-sm navbar-light header">
+          <a className=" navbar-brand" href="/">
+            <img
+              src="https://hn.algolia.com/assets/logo-hn-search.png"
+              alt=""
+              width="40"
+              height="40"
+            />
+            <span style={{ color: "white" }}> Search Hacker News</span>
+          </a>
+
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <i className="fa fa-search  fa-lg search-icon" />
+            </div>
+            <input
+              className="form-control"
+              onChange={this.search}
+              type="search"
+              placeholder="Search story"
+            />
+            {/* <button className="btn btn-outline-success" type="submit">
+            Search
+          </button> */}
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Header1;
